9039597b6688855533e5babc6e712c354ade3a3c
Сергей Саморуков <bauman1922@gmail.com>
2023-08-23 19:02:10 +0300

    добавлен management для csv файла, бд загружена

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -1,6 +1,6 @@
-from pathlib import Path
+import os
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'django-insecure-c5#r9lpk5^8rf82%*c4!=f-(-bg!55z+b(fw7mf!c@k!8*i2p6'
 
@@ -55,7 +55,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -92,3 +92,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "users.User"
+
+
---
+++ backend/foodgram/recipes/management/commands/load_ingredients.py
@@ -0,0 +1,28 @@
+import csv
+from django.core.management import BaseCommand
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if Ingredient.objects.exists():
+            print("Данные уже загружены...выход!")
+            return
+        print("Загрузка Ingredients данных")
+
+        csv_file_path = "C:/Dev/foodgram-project-react/data/ingredients.csv"
+
+        try:
+            with open(csv_file_path, "r", encoding="utf-8") as file:
+                csv_reader = csv.DictReader(file)
+                for row in csv_reader:
+                    db = Ingredient(
+                        name=row["name"],
+                        unit=row["unit"]
+                    )
+                    db.save()
+                print("Ingredients импортированы.")
+        except FileNotFoundError:
+            print("CSV file не найден.")
+        except Exception as e:
+            print(f"Произошла ошибка: {str(e)}")
