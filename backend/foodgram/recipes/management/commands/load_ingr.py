b95b5596d6054c6f90e65213784dd7ea75bd2cf5
Сергей Саморуков <bauman1922@gmail.com>
2023-09-06 16:35:24 +0300

    исправлено management комманда, nginx, docker

--- backend/foodgram/recipes/management/commands/load_ingr.py
+++ backend/foodgram/recipes/management/commands/load_ingr.py
@@ -2,17 +2,29 @@ import csv
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
-
 from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        with open(f'{settings.BASE_DIR}/data/ingredients.csv') as csv_file:
-            file_reader = csv.DictReader(csv_file)
-            for row in file_reader:
-                db = Ingredient(
-                    name=row['name'],
-                    measurement_unit=row['measurement_unit']
-                )
-                db.save()
+        if Ingredient.objects.exists():
+            print("Данные уже загружены...выход!")
+            return
+        print("Загрузка Ingredients данных")
+
+        try:
+            file_path = "foodgram/data/ingredients.csv"
+            print(file_path)
+            with open(file_path) as file:
+                csv_reader = csv.DictReader(file)
+                for row in csv_reader:
+                    db = Ingredient(
+                        name=row["name"],
+                        measurement_unit=row["measurement_unit"]
+                    )
+                    db.save()
+                print("Ingredients импортированы.")
+        except FileNotFoundError:
+            print("CSV file не найден.")
+        except Exception as e:
+            print(f"Произошла ошибка: {str(e)}")
