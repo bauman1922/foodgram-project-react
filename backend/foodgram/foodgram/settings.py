3f42abcf05a283f0bf2f0aef11f4db22cf1f2a88
Сергей Саморуков <bauman1922@gmail.com>
2023-09-07 17:33:44 +0300

    text

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -1,17 +1,23 @@
 import os
 from pathlib import Path
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-c5#r9lpk5^8rf82%*c4!=f-(-bg!55z+b(fw7mf!c@k!8*i2p6'
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ['*']
+
+# SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+
+# DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['*']).split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
